bdf8c238e0711d9c68235c8097e1e6851e767d28
Chris <chrishjimenez@gmail.com>
2018-01-23 18:41:38 -0500

    Complete JS refactor
    Thanks to some help, completely refactored the JS to route all rolls through a single function, effectively reducing 50+ lines of code down to less than 20.

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ var addDie = {
     document.getElementById("multidice").innerHTML = document.getElementById(result).value;
   },
 
-//Tells which die the user has current selected. 
+//Tells which die the user has currently selected.
   indication: function(pickedDice) {
     document.getElementById("selected-die").innerHTML = "";
     document.getElementById("selected-die").innerHTML = pickedDice.value;
@@ -13,46 +13,9 @@ var addDie = {
 }
 
 //Random number gens for each die
-var diceRoll = {
-  rollAll: function(dice){
-    
-  },
-  
-  rolld20: function() {
-    var rolled20 = document.getElementById("results");
-    var rolledNumber = Math.floor(Math.random() * 20) + 1;
+function rollDice(sides) {
+    var rolled = document.getElementById("results");
+    var rolledNumber = Math.floor(Math.random() * sides) + 1;
 
-    if (rolledNumber === 20) {
-      rolled20.innerHTML = "<p>" + rolledNumber + "<br>Nice!</p>";
-    } else if (rolledNumber === 1) {
-      rolled20.innerHTML = "<p>" + rolledNumber + "<br>Oops.</p>";
-    } else {
-      rolled20.innerHTML = "<p>" + rolledNumber + "</p>";
-    }
-  },
-  rolld12: function() {
-    var rolled12 = document.getElementById("results");
-    var rolledNumber = Math.floor(Math.random() * 12) + 1;
-    rolled12.innerHTML = "<p>" + rolledNumber + "</p>";
-  },
-  rolld10: function() {
-    var rolled10 = document.getElementById("results");
-    var rolledNumber = Math.floor(Math.random() * 10) + 1;
-    rolled10.innerHTML = "<p>" + rolledNumber + "</p>";
-  },
-  rolld8: function() {
-    var rolled8 = document.getElementById("results");
-    var rolledNumber = Math.floor(Math.random() * 8) + 1;
-    rolled8.innerHTML = "<p>" + rolledNumber + "</p>";
-  },
-  rolld6: function() {
-    var rolled6 = document.getElementById("results");
-    var rolledNumber = Math.floor(Math.random() * 6) + 1;
-    rolled6.innerHTML = "<p>" + rolledNumber + "</p>";
-  },
-  rolld4: function() {
-    var rolled4 = document.getElementById("results");
-    var rolledNumber = Math.floor(Math.random() * 4) + 1;
-    rolled4.innerHTML = "<p>" + rolledNumber + "</p>";
-  }
-};
+    rolled.innerHTML =  "<p>" + rolledNumber + "</p>";
+}

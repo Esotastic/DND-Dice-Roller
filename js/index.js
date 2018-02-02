06636c4c3a9ee56c0c65d95426d9429cdb1f1deb
Chris <chrishjimenez@gmail.com>
2018-02-01 21:52:05 -0500

    JS Edits
    cleaned up variables in JS; set a longer line into a variable for reuse.

--- js/index.js
+++ js/index.js
@@ -1,19 +1,19 @@
 //Injects multiple results rolls into a div under the main results.
 var addDie = {
   multiDie: function(result) {
+    var extraDice = document.getElementById(result).value;
     document.getElementById("multidice").innerHTML = "";
-    document.getElementById("multidice").innerHTML = document.getElementById(result).value;
+    document.getElementById("multidice").innerHTML = extraDice;
   },
 }
 
 //Random number gen
 function rollDice(sides) {
+  var moreDice = document.getElementById("multidice").innerHTML;
     var rolled = document.getElementById("results");
     var rolledNumber = Math.floor(Math.random() * sides) + 1;
     rolled.innerHTML =  "<p>" + rolledNumber + "</p>";
-
     //Tells which die the user has currently selected.
     document.getElementById("selected-die").innerHTML = "";
     document.getElementById("selected-die").innerHTML = "<p> d" + sides + "</p>";
-
 }

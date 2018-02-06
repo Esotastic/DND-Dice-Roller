08f33235b95cb6b6a53964626bdbc0c8f47deef7
Chris <chrishjimenez@gmail.com>
2018-02-05 22:08:21 -0500

    Multidice displaying in the DOM
    Added functionality to show the extra dice that were chosen to be displayed in the DOM

--- js/index.js
+++ js/index.js
@@ -2,17 +2,25 @@
 var addDie = {
   multiDie: function(result) {
     var extraDice = document.getElementById(result).value;
-    document.getElementById("multidice").innerHTML = "";
-    document.getElementById("multidice").innerHTML = extraDice;
+    // document.getElementById("multidice").innerHTML = "";
+    // document.getElementById("multidice").innerHTML = extraDice;
   },
 }
 
 //Random number gen
 function rollDice(sides) {
-  var moreDice = document.getElementById("multidice").innerHTML;
-    var rolled = document.getElementById("results");
-    var rolledNumber = Math.floor(Math.random() * sides) + 1;
+  var moreDice = document.getElementById("dicemultiply").value;
+  var rolled = document.getElementById("results");
+  var multiInner = document.getElementById("multidice").innerHTML;
+  var rolledNumber = Math.floor(Math.random() * sides) + 1;
+
+  if (moreDice > 0){
+    document.getElementById("multidice").innerHTML = "<p>" + (rolledNumber * moreDice) + "</p>";
+  } else {
+    document.getElementById("multidice").innerHTML = "";
     rolled.innerHTML =  "<p>" + rolledNumber + "</p>";
+ }
+
     //Tells which die the user has currently selected.
     document.getElementById("selected-die").innerHTML = "";
     document.getElementById("selected-die").innerHTML = "<p> d" + sides + "</p>";
